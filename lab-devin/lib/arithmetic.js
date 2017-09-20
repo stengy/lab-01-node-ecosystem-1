a8d4fad241ca606610fad301a2f2bf0991be94ff
Devin G <engy100x@gmail.com>
2017-09-19 21:10:20 -0700

    test written for subtract, arithmetic methods written, all tests passing

--- lab-devin/lib/arithmetic.js
+++ lab-devin/lib/arithmetic.js
@@ -5,6 +5,19 @@ const arithmetic = module.exports = {};
 arithmetic.add = function (a,b) {
 
   if(typeof a !== 'number' || typeof b !== 'number') {
-    return null
-  } else
-}
+    return null;
+  } else {
+    let total = a + b;
+    return total;
+  }
+};
+
+arithmetic.sub = function (a,b) {
+
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    return null;
+  } else {
+    let total = a - b;
+    return total;
+  }
+};
--- lab-devin/test/arithmetic.test.js
+++ lab-devin/test/arithmetic.test.js
@@ -9,8 +9,9 @@ describe('Arithmetic', () => {
   describe('testing add', () => {
 
     it('should require two numbers', () => {
+
       let first = arithmetic.add('nein', 9);
-      let second = arithmetic.add(1, 'ein');
+      let second = arithmetic.add(1, 'bist');
 
       expect(first).toBe(null);
       expect(second).toBe(null);
@@ -18,15 +19,49 @@ describe('Arithmetic', () => {
     });
 
     it('should require two inputs', () => {
+
       let result = arithmetic.add(3);
+
+      expect(result).toBe(null);
+
+    });
+
+    it('should add two numbers', () => {
+
+      let result = arithmetic.add(4,20);
+
+      expect(result).toEqual(24);
+
+    });
+  });
+
+  describe('testing subtract', () => {
+
+    it('should require two inputs', () => {
+
+      let result = arithmetic.sub(13);
 
       expect(result).toBe(null);
     });
 
-    it('should add two numbers', () => {
-      let result = arithmetic.add(4,20);
+    it('should require two numbers', () => {
 
-      expect(result).toEqual(24);
+      let sub = 'placeholder';
+      let first = arithmetic.sub('brrrt', 10);
+      let second = arithmetic.sub(12, 'skrrt');
+      let fn = arithmetic.sub(21, sub);
+
+      expect(first).toBe(null);
+      expect(second).toBe(null);
+      expect(fn).toBe(null);
+    });
+
+    it('should subtract two numbers', () => {
+
+      let result = arithmetic.sub(4,1);
+
+      expect(result).toEqual(3);
+      
     });
   });
 });
